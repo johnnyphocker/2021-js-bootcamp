1a4393cbe7761676921f099527d66bb36207f07b
johnnyphocker <johnny.phocker@gmail.com>
2021-07-10 15:26:39 -0500

    Se añadió el script de JS

---
+++ sesion02/js/main.js
@@ -0,0 +1,45 @@
+// TIPOS DE VALORES PARA LAS VARIABLES
+
+var text = 'Hola mundo'; // string
+var num = 10; // number
+var bool = true; // true || false
+var plate = null;
+var und; // undefined
+
+
+var age = 18;
+var user = 'juanpablo';
+
+// OPERADORES CONDICIONALES
+// ===
+// <
+// >
+// >=
+// <=
+// !=
+
+console.log(user.indexOf('z'))
+
+if (age >= 21) {
+  console.log('Puede pasar al club');
+} else {
+  console.log('Lo siento, te quedas afuera');
+};
+
+if (user.length < 8) {
+  console.log('Tu usuario debe tener más 7 caracteres');
+} else if (user.indexOf(' ') >= 0) {
+  console.log('Tu usuario no sebe tener espacios vacíos');
+} else {
+  console.log('Has creado una cuenta');
+}
+
+
+
+
+
+
+
+
+
+
