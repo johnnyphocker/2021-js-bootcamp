ff53fe224cb95a5ddfcc9595072c50c0f0cab7f4
johnnyphocker <johnny.phocker@gmail.com>
2021-07-31 12:29:02 -0600

    Created a site with bootstrap

---
+++ sesion05/main.js
@@ -0,0 +1,19 @@
+// let wrapper = document.querySelector('#wrapper')
+// let btn = document.querySelector('#btn')
+// let alert = document.querySelector('[data-alert="primary"]')
+
+// alert.style.display = 'none';
+
+// btn.addEventListener('click', () => {
+//   alert.style.display = 'block';
+//   setTimeout(() => {
+//     alert.style.display = 'none';
+//   }, 2000);
+// });
+
+// btn.onclick = () => {
+//   console.log('Presionado usando evento onclick');
+// };
+
+
+feather.replace();
